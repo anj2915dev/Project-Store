e6355117d86b5a9fc4faf6fa67c181570dbc7e59
John Doe <alirezanjprogrammer110@gmail.com>
2021-09-15 15:57:23 +0430

    show product in html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,61 @@
+const productCenter = document.querySelector(".product-center")
+
+import { Products } from "./productData.js";
 
 
+
+//  read product 
+class products{
+    get readProduct() {
+        return Products;
+    }
+}
+
+
+// show product in html
+
+class UI {
+
+    showProduct(pruducts) {
+        let result = "";
+        pruducts.forEach(item => {
+            result += ` <section class="product">
+            <div class="image-container">
+                <img src="${item.imageUrl}" alt="p -1" class="product-image">
+            </div>
+            <div class="product-des">
+                <p class="product-title"> <strong>${item.title}</strong> </p><br>
+
+                <p class=" discont-price"> <span class="discount">33%</span> <sub><del>30000</del> </sub> </p>
+                <p class="product-price">
+
+                    <strong>
+                        ${item.price}
+                    </strong>
+                </p>
+            </div>
+            <!-- <div class="btn-box"> -->
+            <button class="add-to-cart" data-id=${item.id}> add to cart </button>
+            <!-- </div> -->
+
+        </section>`
+            productCenter.innerHTML = result;
+
+
+
+        })
+    }
+
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", event => {
+    const pruduct = new products();
+    const productData=pruduct.readProduct
+    const ui=new UI()
+    ui.showProduct(productData)
+
+})
 
--- js/productData.js
+++ js/productData.js
@@ -1,1 +1,50 @@
+export const Products = [
+    {
+        id: 1,
+        title: "takht1",
+        price: 1111,
+        imageUrl: "./images/takht-1.jpg.crdownload"
+
+    },
+    {
+        id: 6,
+        title: "takht1",
+        price: 1111,
+        imageUrl: "./images/takht-1.jpg.crdownload"
+
+    },
+    {
+        id: 2,
+        title: "takht22",
+        price: 2222,
+        imageUrl: "./images/takht-1.jpg.crdownload"
+
+
+    },
+    {
+        id: 3,
+        title: "takht33",
+        price: 3333,
+        imageUrl: "./images/takht-1.jpg.crdownload"
+
+
+    },
+    {
+        id: 4,
+        title: "takht44",
+        price: 4444,
+        imageUrl: "./images/takht-2.jpg"
+
+
+    },
+    {
+        id: 5,
+        title: "takht55",
+        price: 5555,
+        imageUrl: "./images/takht-1.jpg.crdownload"
+
+
+    }
+
+]
 
